39da37e4638e0e11edb49a40801471e3b5cb58c8
何大洋 <he.jinyang@foxmail.com>
2019-04-09 18:01:25 +0800

    压测

--- src/main/java/com/hjy/miaosha/MiaoshaApplication.java
+++ src/main/java/com/hjy/miaosha/MiaoshaApplication.java
@@ -2,12 +2,19 @@ package com.hjy.miaosha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MiaoshaApplication {
+public class MiaoshaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MiaoshaApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MiaoshaApplication.class);
     }
 
 }
